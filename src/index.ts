c41a1418cb8f83d9740e885497e11a2b6c9da12d
Leo4815162342 <leonid.pyrlya@gmail.com>
2019-09-01 13:21:08 +0200

    cleanup index

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
 import MyfxbookApi from './myfxbook-api';
-import * as interfaces from './myfxbook-api.interfaces';
-export { MyfxbookApi, interfaces };
 
-const myfx = new MyfxbookApi({
-  email: '',
-  password: ''
-});
-
-myfx
-  .getDailyData(631370, '2013-03-22', '2013-03-27')
-  .then(data => console.log(JSON.stringify(data)))
-  .catch(err => console.log(err));
+export { MyfxbookApi };
